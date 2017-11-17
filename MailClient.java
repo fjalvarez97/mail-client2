0897295425524e941f11df51b9fd9ff6cf53fbd2
Fran Alvarez <franjalvarez955@gmail.com>
2017-11-17 19:15:18 +0100

    Ahora se puede saber el numero de mensajes enviados/recibidos, y la longitud del mensaje mas largo y por quien es este enviado

--- MailClient.java
+++ MailClient.java
@@ -11,7 +11,12 @@ public class MailClient
     private MailServer server;
     // The user running this client.
     private String user;
-
+    // Number of sent messages.
+    private int mensajesEnviados;
+    //Number of read messages:
+    private int mensajesRecibidos;
+    //Saves the longest message;
+    private MailItem mensajeMasLargo;
     /**
      * Create a mail client run by user and attached to the given server.
      */
@@ -19,6 +24,9 @@ public class MailClient
     {
         this.server = server;
         this.user = user;
+        mensajesEnviados = 0;
+        mensajesRecibidos= 0;
+        mensajeMasLargo = new MailItem("","","","");
     }
 
     /**
@@ -26,6 +34,7 @@ public class MailClient
      */
     public MailItem getNextMailItem()
     {
+        mensajesRecibidos += 1;
         return server.getNextMailItem(user);
     }
 
@@ -40,7 +49,12 @@ public class MailClient
             System.out.println("No new mail.");
         }
         else {
-            item.print();
+            item.print(); 
+            if (item.getMessage().length() > mensajeMasLargo.getMessage().length())
+            {   //Saves the messages if its longer than the actually longest one.
+                mensajeMasLargo = item;
+                mensajesRecibidos += 1;
+            }
         }
     }
 
@@ -53,7 +67,23 @@ public class MailClient
     public void sendMailItem(String to, String message, String subject)
     {
         MailItem item = new MailItem(user, to,subject, message);
-        
         server.post(item);
+        mensajesEnviados += 1;
+    }
+    
+    public String numeroMensajes()
+    {
+        //Return number of sent/received messages and the longest message you received, and the email that sent it.
+        String mensajeDevolver;
+        if(mensajesRecibidos > 0 )
+        {
+           mensajeDevolver = "Recibidos =" + mensajesRecibidos + " " + "Enviados = " + mensajesEnviados + " El mensaje mas Largo tiene " +
+           mensajeMasLargo.getMessage().length() + " Caracteres, enviado por la direccion de correo: " + mensajeMasLargo.getFrom()  ;
+        }
+        else
+        {
+           mensajeDevolver = "Recibidos =" + mensajesRecibidos + " " + "Enviados = " + mensajesEnviados;
+        }
+        return mensajeDevolver;
     }
 }
